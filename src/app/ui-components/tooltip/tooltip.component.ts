b7d421b81aa82891760a21ba36a7cdf89cd0d5a2
aoweeks <aoweeks@gmail.com>
2020-04-20 16:20:19 +0100

    Generate tooltip component

--- src/app/ui-components/tooltip/tooltip.component.ts
+++ src/app/ui-components/tooltip/tooltip.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-tooltip',
+  selector: 'aw-tooltip',
   templateUrl: './tooltip.component.html',
   styleUrls: ['./tooltip.component.scss']
 })
 export class TooltipComponent implements OnInit {
+
+  @Input() text = '';
 
   constructor() { }
 
