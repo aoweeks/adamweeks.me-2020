7bd273790c673bad67cf9b53b3b099d2b5204d13
aoweeks <aoweeks@gmail.com>
2020-04-21 22:17:44 +0100

    Adjust tooltip to right if would extend past left of window

--- src/app/ui-components/tooltip/tooltip.component.ts
+++ src/app/ui-components/tooltip/tooltip.component.ts
@@ -1,17 +1,38 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef, AfterViewInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'aw-tooltip',
   templateUrl: './tooltip.component.html',
   styleUrls: ['./tooltip.component.scss']
 })
-export class TooltipComponent implements OnInit {
+export class TooltipComponent implements AfterViewInit {
 
   @Input() text = '';
+  @ViewChild('tooltipBody') tooltip: ElementRef;
+
+  @HostListener('window:resize')
+  onWindowResize() {
+    this.ensureTooltipWithinWindow();
+  }
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.ensureTooltipWithinWindow();
   }
 
+
+  /*  If the tooltip would extend out past the left of the browser window,
+      move accordingly */
+  ensureTooltipWithinWindow(): void {
+    setTimeout( () => { // In setTimeout to circumvent angular error
+      const absoluteLeft: number = this.tooltip.nativeElement.getBoundingClientRect().left;
+      const relativeLeft: number = parseInt(window.getComputedStyle(this.tooltip.nativeElement).left, 10);
+
+      if (absoluteLeft < 0) {
+        const adjustedLeft = relativeLeft + -absoluteLeft + 10;
+        this.tooltip.nativeElement.style.left = `${adjustedLeft}px`;
+      }
+    }, 0);
+  }
 }
