4e8c900553f3c701d79d2bac08199acd8c0c062c
aoweeks <aoweeks@gmail.com>
2020-04-22 03:19:45 +0100

    Implement animate on scroll directive functionality

--- src/app/directives/animate-on-scroll.directive.ts
+++ src/app/directives/animate-on-scroll.directive.ts
@@ -1,10 +1,53 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Renderer2, AfterViewInit } from '@angular/core';
 
 @Directive({
-  selector: '[appAnimateOnScroll]'
+  selector: '[awAnimateOnScroll]'
 })
-export class AnimateOnScrollDirective {
+export class AnimateOnScrollDirective implements AfterViewInit {
 
-  constructor() { }
+  elTop: number;
+  triggerPoint: number;
+
+
+  scrollListener = this.renderer.listen(window, 'scroll', event => {
+    this.checkIfElementInView();
+  });
+
+  resizeListener = this.renderer.listen(window, 'resize', event => {
+    // console.log('resized');
+    this.elTop = this.el.nativeElement.offsetTop;
+    this.triggerPoint = this.calculateTriggerPoint();
+    this.checkIfElementInView();
+  });
+
+  constructor(private el: ElementRef,
+              private renderer: Renderer2) {
+  }
+
+  ngAfterViewInit(): void {
+    this.elTop = this.el.nativeElement.offsetTop;
+    this.triggerPoint = this.calculateTriggerPoint();
+
+    if (this.elTop - window.innerHeight > window.pageYOffset) {
+      this.renderer.addClass(this.el.nativeElement, 'waiting-for-scroll');
+    } else {
+      this.elementIsInView();
+    }
+  }
+
+  checkIfElementInView() {
+    if (window.pageYOffset > this.triggerPoint) { this.elementIsInView(); }
+  }
+
+  elementIsInView() {
+    this.renderer.removeClass(this.el.nativeElement, 'waiting-for-scroll');
+    this.scrollListener();
+    this.resizeListener(); // Destroy listeners
+  }
+
+  calculateTriggerPoint(): number {
+    return this.elTop - window.innerHeight + 90;
+  }
+
 
 }
